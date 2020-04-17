2f2b0404a39031ee2508bd26ce44309654f838d4
Howard Daniels <hdaniels13tat@gmail.com>
2020-04-17 18:26:27 -0400

    updated links on navbar

--- portfolioapp/src/Navbar/index.js
+++ portfolioapp/src/Navbar/index.js
@@ -16,22 +16,6 @@ function Navbar() {
       </li>
       <li className="nav-item">
         <Link
-          to="/about"
-          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/blog"
-          className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
-        >
-          Blog
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
           to="/contact"
           className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
         >
@@ -40,10 +24,10 @@ function Navbar() {
       </li>
       <li className="nav-item">
         <Link
-          to="/contact/learn"
-          className={location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
+          to="/portfolio"
+          className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
         >
-          Learn
+          Portfolio
         </Link>
       </li>
     </ul>
