5c6d385bed55f9b2aca28a7820254ec90260b225
Howard Daniels <hdaniels13tat@gmail.com>
2020-04-18 18:18:13 -0400

    set up home folder and other components

--- portfolioapp/src/App.js
+++ portfolioapp/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router} from "react-router-dom";
-import Navbar from './Navbar';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Wrapper from "./components/Wrapper";
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -9,7 +14,12 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Wrapper>
+        <Footer />
       </div>
     </Router>
   );
