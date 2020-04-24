144203b8fe06319031f9b65d07467cd47e139d58
Howard Daniels <hdaniels13tat@gmail.com>
2020-04-23 23:07:08 -0400

    made bio image visible

--- portfolioapp/src/App.js
+++ portfolioapp/src/App.js
@@ -7,6 +7,8 @@ import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Footer from './components/Footer';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
